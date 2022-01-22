926d83f0003e1ce7b08ce5f245e8b8bcd7be5294
kshneo0 <kshneo@gmail.com>
2022-01-22 12:05:22 +0900

    sec3 ch014 bootstrap

--- react-app/test-app/src/index.js
+++ react-app/test-app/src/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import AppFooter from "./AppFooter";
+import AppContent from "./AppContent";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
 class App extends Component {
@@ -9,6 +12,7 @@ class App extends Component {
       <div className="app">
         <div>
           <h1>Hello, world!</h1>
+          <AppContent />
         </div>
         <AppFooter />
       </div>
