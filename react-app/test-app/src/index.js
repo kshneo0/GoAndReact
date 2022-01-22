3a8335a0ee8e2966282b7fab77b1c30117ae7a99
kshneo0 <kshneo@gmail.com>
2022-01-22 11:19:09 +0900

    sec3 ch012

---
+++ react-app/test-app/src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import AppFooter from "./AppFooter";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Hello, world!</h1>
+        </div>
+        <AppFooter />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
