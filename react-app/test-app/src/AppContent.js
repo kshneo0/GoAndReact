afb24412d5f54ba17038ae8a3009a72abd38000c
kshneo0 <kshneo@gmail.com>
2022-01-22 18:18:08 +0900

    sec3 ch019 More events

--- react-app/test-app/src/AppContent.js
+++ react-app/test-app/src/AppContent.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
 
 export default class AppContent extends Component {
+  anotherFunction = () => {
+    console.log("another function");
+  };
+
+  leftParagraph = () => {
+    console.log("left the paragraph");
+  };
+
   fetchList = () => {
+    //fetchList() {
     // console.log("I was clicked");
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((json) => {
         console.log(json);
-
+        //this.anotherFunction();
         let posts = document.getElementById("post-list");
-        json.forEach(function(obj){
-            let li = document.createElement("li");
-            li.appendChild(document.createTextNode(obj.title));
-            posts.appendChild(li);
-        })
+        json.forEach(function (obj) {
+          let li = document.createElement("li");
+          li.appendChild(document.createTextNode(obj.title));
+          posts.appendChild(li);
+        });
       });
   };
   render() {
@@ -21,6 +30,13 @@ export default class AppContent extends Component {
       <p>
         This is the content.
         <br />
+        <hr />
+        <p
+          onMouseEnter={this.anotherFunction}
+          onMouseLeave={this.leftParagraph}
+        >
+          This is come text
+        </p>
         <button onClick={this.fetchList} className="btn btn-primary">
           Fetch Data
         </button>
