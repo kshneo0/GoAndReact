bd18328f69bbb703bcc6580e87212bc17db9448a
kshneo0 <kshneo@gmail.com>
2022-01-22 12:13:16 +0900

    sec3 ch015 bootstrap exam

--- react-app/test-app/src/index.js
+++ react-app/test-app/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import AppFooter from "./AppFooter";
 import AppContent from "./AppContent";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 
 class App extends Component {
