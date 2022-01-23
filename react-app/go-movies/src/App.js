23a2a8dbf93250a9c1c7ae9c1eeb4a5133b130d5
kshneo0 <kshneo@gmail.com>
2022-01-23 14:58:45 +0900

    sec4 ch033 More about routing Part

--- react-app/go-movies/src/App.js
+++ react-app/go-movies/src/App.js
@@ -5,10 +5,12 @@ import {
   Route,
   Link,
   useParams,
+  useRouteMatch,
 } from "react-router-dom";
 import Movies from "./components/Movies";
 import Admin from "./components/Admin";
 import Home from "./components/Home";
+import Categories from "./components/Categories";
 
 export default function App() {
   return (
@@ -29,6 +31,9 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/by-category">Categories</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Manage Catalogue</Link>
                 </li>
               </ul>
@@ -42,6 +47,21 @@ export default function App() {
               <Route path="/movies">
                 <Movies />
               </Route>
+              <Route exact path="/by-category">
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path="/by-category/drama"
+                render={(props) => <Categories {...props} title={`Drama`} />}
+              />
+
+              <Route
+                exact
+                path="/by-category/comedy"
+                render={(props) => <Categories {...props} title={`Comedy`} />}
+              />
+
               <Route path="/admin">
                 <Admin />
               </Route>
@@ -59,4 +79,22 @@ export default function App() {
 function Movie() {
   let { id } = useParams();
   return <h2>Movie id {id}</h2>;
+}
+
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul>
+        <li>
+          <Link to={`${path}/comedy`}>Comedy</Link>
+        </li>
+        <li>
+          <Link to={`${url}/drama`}>Drama</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
