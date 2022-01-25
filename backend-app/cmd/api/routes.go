a1adf3777c500f6ce65a3d0711e63d9d328ab3c0
kshneo0 <kshneo@gmail.com>
2022-01-25 10:40:14 +0900

    sec7 ch072 Implementing delete on the back end

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -6,18 +6,18 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-
-func (app * application) routes() http.Handler {
+func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
-	router.HandlerFunc(http.MethodGet,"/v1/movie/:id", app.getOneMovie)
-	router.HandlerFunc(http.MethodGet,"/v1/movies", app.getAllMovies)
-	router.HandlerFunc(http.MethodGet,"/v1/movies/:genre_id", app.getAllMoviesByGenre)
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 
-	router.HandlerFunc(http.MethodGet,"/v1/genres", app.getAllGenres)
+	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 
-	router.HandlerFunc(http.MethodPost,"/v1/admin/editmovie", app.editMovie)
+	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCORS(router)
 }
