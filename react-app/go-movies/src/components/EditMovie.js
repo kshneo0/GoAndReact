50a085c01913b0af717832433a12742a87d76b2c
kshneo0 <kshneo@gmail.com>
2022-01-25 11:15:21 +0900

    sec7 ch075 displaying list of movies to edit

--- react-app/go-movies/src/components/EditMovie.js
+++ react-app/go-movies/src/components/EditMovie.js
@@ -72,9 +72,12 @@ export default class EditMovie extends Component {
             alert: { type: "alert-danger", message: data.error.message },
           });
         } else {
-          this.setState({
-            alert: { type: "alert-success", message: "Changes saved!" },
+          this.props.history.push({
+            pathname: "/admin",
           });
+          // this.setState({
+          //   alert: { type: "alert-success", message: "Changes saved!" },
+          // });
         }
       });
   };
